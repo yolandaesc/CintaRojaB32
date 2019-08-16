4bc810e3a8a54ab1e90de972a4ab586e61d445f6
yolandaesc <53450609+yolandaesc@users.noreply.github.com>
2019-08-15 20:19:51 -0500

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,48 +1,48 @@
 const express = require("express");
 const path = require('path');
 const router = express.Router();
-const { Vehiculo } = require("../models/vehiculo");
+const { Movie } = require("../models/movie");
 
 router.get("/", (req, res, next) => {
     res.status(200).sendFile(path.resolve("src/views/index.html"));
 });
 
 // CREATE
-router.post("/api/v1/vehiculo", (req, res, next) => {
-    const { nombre, year, kilometraje, description, ultServicio, image } = req.body;
-    const newVehiculo = Vehiculo({
-        nombre,
+router.post("/api/v1/pelicula", (req, res, next) => {
+    const { title, year, description, image, theme, director } = req.body;
+    const newMovie = Movie({
+        title,
         year,
-        kilometraje,
         description,
-        ultServicio,
-        image
+        image,
+        theme,
+        director
     });
-    newVehiculo.save((err, vehiculo) => {
-        err ? res.status(409).send(err.message) : res.status(201).send(vehiculo)
+    newMovie.save((err, pelicula) => {
+        err ? res.status(409).send(err.message) : res.status(201).send(pelicula)
     });
 });
 
 // READ
-router.get("/api/v1/vehiculo", (req, res, next) => {
-    Vehiculo.find().exec()
-        .then(vehiculos => res.status(200).send(vehiculos))
+router.get("/api/v1/pelicula", (req, res, next) => {
+    Movie.find().exec()
+        .then(movies => res.status(200).send(movies))
         .catch(err => res.status(404).send(err));
 });
 
-router.get("/api/v1/vehiculoById/", (req, res, next) => {
+router.get("/api/v1/peliculaById/", (req, res, next) => {
     const { id } = req.query;
-    Vehiculo.findById(id).exec()
-        .then(vehiculos => res.status(200).send(vehiculos))
+    Movie.findById(id).exec()
+        .then(movies => res.status(200).send(movies))
         .catch(err => res.status(404).send(err));
 });
 
-router.get("/api/v1/busqueda/vehiculo", (req, res, next) => {
+router.get("/api/v1/busqueda/pelicula", (req, res, next) => {
     const { q } = req.query;
-    Vehiculo.find({ title: q }).exec()
-        .then(vehiculo => {
-            vehiculo.length > 0
-                ? res.status(200).send(vehiculo)
+    Movie.find({ title: q }).exec()
+        .then(movie => {
+            movie.length > 0
+                ? res.status(200).send(movie)
                 : res.status(404).send("Not found")
         })
         .catch(err => res.status(404).send(err))
@@ -51,14 +51,14 @@ router.get("/api/v1/busqueda/vehiculo", (req, res, next) => {
 // UPDATE
 
 // Modificar todo el objeto o registro
-router.put("/api/v1/vehiculo/update", (req, res, next) => {
+router.put("/api/v1/peliculas/update", (req, res, next) => {
     const { id } = req.query;
     const body = req.body;
 
-    Vehiculo.findByIdAndUpdate(id, { $set: body }, { new: true })
-        .then(newVehiculo => {
-            if (newVehiculo !== null) {
-                res.status(202).send(newVehiculo)
+    Movie.findByIdAndUpdate(id, { $set: body }, { new: true })
+        .then(newMovie => {
+            if (newMovie !== null) {
+                res.status(202).send(newMovie)
             } else {
                 res.status(304).send("Registro no encontrado, imposible modificar")
             }
@@ -67,14 +67,14 @@ router.put("/api/v1/vehiculo/update", (req, res, next) => {
 });
 
 // Modificar parcialmente el registro
-router.patch("/api/v1/vehiculo/update", (req, res, next) => {
+router.patch("/api/v1/peliculas/update", (req, res, next) => {
     const { id } = req.query;
     const body = req.body;
 
-    Vehiculo.findByIdAndUpdate(id, { $set: body }, { new: true })
-        .then(newVehiculo => {
-            if (newVehiculo !== null) {
-                res.status(202).send(newVehiculo)
+    Movie.findByIdAndUpdate(id, { $set: body }, { new: true })
+        .then(newMovie => {
+            if (newMovie !== null) {
+                res.status(202).send(newMovie)
             } else {
                 res.status(304).send("Registro no encontrado, imposible modificar")
             }
@@ -83,13 +83,13 @@ router.patch("/api/v1/vehiculo/update", (req, res, next) => {
 });
 
 // DELETE
-router.delete("/api/v1/vehiculo/delete", (req, res, next) => {
+router.delete("/api/v1/peliculas/delete", (req, res, next) => {
     const { id } = req.query;
 
-    Vehiculo.findByIdAndRemove(id).exec()
-        .then(vehiculo => {
-            vehiculo !== null
-                ? res.status(200).send({ mensaje: "Vehiculo borrado exitosamente", body: vehiculo })
+    Movie.findByIdAndRemove(id).exec()
+        .then(pelicula => {
+            pelicula !== null
+                ? res.status(200).send({ mensaje: "Película borrada exitosamente", body: pelicula })
                 : res.status(304).send({ mensaje: "Registro no eliminado " })
         })
         .catch(err => res.status(304).send({ mensaje: "Registro no eliminado " }))
